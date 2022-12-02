9ffa8b78f137b3afc21a7da2cc6a48e3f3bb2951
Ray Hsu <71971775+wavelolz@users.noreply.github.com>
2022-12-02 12:57:16 +0800

    Update rod cut dp.cpp

--- rod cut dp.cpp
+++ rod cut dp.cpp
@@ -7,12 +7,22 @@ int max(int a, int b){return (a >= b) ? a : b;}
 vector<vector<int > > rodcut(vector<int> price, int n);
 int main(){
 
-    vector<int> price {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-    int n = price.size();
-    vector<vector<int> > a = rodcut(price, n);
+    int N;
+    cin >> N;
+    int arr[N];
+    for (int i = 0; i < N; i ++){
+        cin >> arr[i];
+    }
+    vector<int> price(N);
+    for (int i = 0; i < N; i ++){
+        price[i] = arr[i];
+    }
+    vector<vector<int> > a = rodcut(price, N);
+
+
     vector<int> cutway;
-    cutway.reserve(n);
-    int curr_len = n;
+    cutway.reserve(N);
+    int curr_len = N;
     int countc = 0;
     while (curr_len >= 1){
         int cutpoint = a.at(1).at(curr_len-1);
@@ -20,23 +30,24 @@ int main(){
         cutway[countc] = cutpoint;
         countc ++;
     }
-    cout << a[0][n] << endl;
+    cout << a[0][N] << endl;
     cout << countc << endl;
-    cout << n << " = ";
-    for (int i = 0; i < countc; i ++){
-        cout << cutway[i] << " ";
+    cout << N << " = ";
+    for (int i = 0; i < countc-1; i ++){
+        cout << cutway[i] << "+";
     }
-
+    cout << cutway[countc-1];
 
 }
 
 
 vector<vector<int> > rodcut(vector<int> price, int n){
-    price.insert(price.begin(), 0);
+    price.insert(price.begin(), 0); // 先在 price 加上長度為 0 的情況
     vector<vector<int> > result;
-    result.resize(2, vector<int>(n+1));
+    result.resize(2, vector<int>(n+1)); // 第一個 row 存價錢, 從 0~N
+                                        // 第二個 row 存切點, 從 0~N
     result[0][0] = 0;
-    result[0][1] = 1;
+    result[0][1] = price[1];
     result[1][0] = 0;
     result[1][1] = 1;
 
@@ -52,6 +63,7 @@ vector<vector<int> > rodcut(vector<int> price, int n){
         }
         result.at(0).at(i) = maxprice;
     }
+
     result[0].erase(result[0].begin());
     result[1].erase(result[1].begin());
 
