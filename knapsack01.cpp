65f99e0d55952bb47f9c9ae8734750989c903353
Ray Hsu <71971775+wavelolz@users.noreply.github.com>
2022-12-16 13:33:36 +0800

    Update knapsack01.cpp

--- knapsack01.cpp
+++ knapsack01.cpp
@@ -3,54 +3,67 @@
 #include <limits.h>
 using namespace std;
 
-pair<vector<vector<int> >, vector<vector<int> >> knapsack(int n, int w, vector<int> weight, vector<int> value);
+pair<int, vector<int>> knapsack(int W, int N, int* weight, int* value);
 int main(){
-    int N = 7;
-    int W = 170;
-    vector<int> weight = {41,50,49,59,55,57,60};
-    vector<int> value = {170,442,525,511,593,546,617};
-    pair<vector<vector<int> >, vector<vector<int> >> ans = knapsack(N, W, weight, value);
-    vector<int> take;
-    int cw = W;
-    for (int i = N; i > 0; i --){
-        if (ans.second.at(i).at(cw) == 1){
-            take.push_back(i);
-            cw -= weight.at(i-1);
-        }
+    int W, N;
+    cin >> W >> N;
+    int weight[N], value[N];
+    for (int i = 0; i < N; i ++){
+        cin >> weight[i];
     }
-    reverse(take.begin(), take.end());
-    cout << "最大可獲得價值為: " << ans.first.at(N).at(W) << endl;
-    cout << "要拿的物品為: ";
-    for (int i = 0; i < take.size(); i ++){
-        cout << take.at(i) << " ";
+    for (int i = 0; i < N; i ++){
+        cin >> value[i];
+    }
+    pair<int, vector<int> > ans = knapsack(W, N, weight, value);
+    cout << ans.first << endl;
+    for (int i = 0; i < ans.second.size(); i ++){
+        cout << ans.second[i] << " ";
     }
 }
-
-pair<vector<vector<int> >, vector<vector<int> >> knapsack(int n, int w, vector<int> weight, vector<int> value){
-    vector<vector<int> > arr;
-    arr.resize((n+1), vector<int>(w+1));
-    vector<vector<int> > takeornot;
-    takeornot.resize((n+1), vector<int>(w+1));
-    for (int i = 1; i <= n; i ++){
-        int maxvalue = INT_MIN;
-        for (int j = 1; j <= w; j ++){
-            if (j - weight[i-1] < 0){
-                arr.at(i).at(j) = arr.at(i-1).at(j); // 確認多放一個後重量夠不夠，不夠的話最佳解就會是少一個物品的最佳解
-            }
-            else{
-                // 決定要不要把這個物品放進去
-                arr.at(i).at(j) = max(arr.at(i-1).at(j), value.at(i-1) + arr.at(i-1).at(j-weight.at(i-1)));
-                if (arr.at(i).at(j) == arr.at(i-1).at(j)){
-                    takeornot.at(i).at(j) = 0;
+pair<int, vector<int>> knapsack(int W, int N, int* weight, int* value){
+    int current_weight = W, inx_item = N;
+    int** arr; int** isselect;
+    vector<int> index;
+    arr = new int* [N+1];
+    isselect = new int* [N+1];
+    for (int i = 0; i <= N; i ++){
+        arr[i] = new int [W+1];
+        isselect[i] = new int [W+1];
+    }
+    for (int i = 0; i <= N; i ++){
+        arr[i][0] = 0;
+        isselect[i][0] = 0;
+    }
+    for (int i = 0; i <= W; i++){
+        arr[0][i] = 0;
+        isselect[0][i] = 0;
+    }
+    for (int i = 1; i <= N; i ++){
+        for (int j = 1; j <= W; j ++){
+            if (j < weight[i-1]){
+                arr[i][j] = arr[i-1][j];
+                isselect[i][j] = 0;
+            } else {
+                if (arr[i-1][j] < value[i-1] + arr[i-1][j-weight[i-1]]){
+                    arr[i][j] = value[i-1] + arr[i-1][j-weight[i-1]];
+                    isselect[i][j] = 1;
                 } else {
-                    takeornot.at(i).at(j) = 1;
+                    arr[i][j] = arr[i-1][j];
+                    isselect[i][j] = 0;
                 }
             }
-            //cout << arr[i][j] << " ";
         }
-        //cout << endl;
     }
-    pair<vector<vector<int> >, vector<vector<int> >> ans = make_pair(arr, takeornot);
+    while (current_weight > 0 && inx_item > 0){
+        if (isselect[inx_item][current_weight] == 1){
+            index.push_back(inx_item);
+            current_weight -= weight[inx_item-1];
+            inx_item -= 1;
+        } else {
+            inx_item -= 1;
+        }
+    }
+    sort(index.begin(), index.end());
+    pair<int, vector<int> > ans = make_pair(arr[N][W], index);
     return ans;
-
 }
