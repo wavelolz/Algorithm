1a56ee0df272292de8891fcac58732cc008fbd6d
Ray Hsu <71971775+wavelolz@users.noreply.github.com>
2022-12-18 13:40:25 +0800

    Update activity selection.cpp

--- activity selection.cpp
+++ activity selection.cpp
@@ -1,76 +1,49 @@
 #include <iostream>
-#include <vector>
 #include <bits/stdc++.h>
-#include <utility>
-#include <algorithm>
-
 using namespace std;
 
-
-vector<pair<int, int>> activityselection(vector<int> s, vector<int> e, int n);
-bool sortbysec(const pair<int, int> a, const pair<int, int> b){return a.second <= b.second;}
-bool sortdesc(const pair<int, int> a, const pair<int, int>b){return a.first >= b. first;}
+vector<int> actselection(int N, int* s, int* e);
+bool sortbysec(pair<int, int> a, pair<int, int> b){return a.second < b.second;}
 int main(){
     int N;
     cin >> N;
-    vector<int> s(N);
-    vector<int> e(N);
-    for (int i = 0; i < N; i ++){
+    int s[N], e[N];
+    for(int i = 0; i < N; i ++){
         cin >> s[i];
-
     }
     for (int i = 0; i < N; i ++){
         cin >> e[i];
     }
-    vector<pair<int, int>> ans = activityselection(s, e, N);
-    cout << endl;
+    vector<int> ans;
+    ans = actselection(N, s, e);
     cout << ans.size() << endl;
-    vector<int>::iterator it;
-    int pos;
-    vector<int> output;
-    for (int i = 0; i < ans.size(); i ++){
-        it = find(s.begin(), s.end(), ans[i].first);
-        pos = distance(s.begin(), it)+1;
-        output.push_back(pos);
+    sort(ans.begin(), ans.end());
+    for(int i = 0; i < ans.size(); i ++){
+        cout << ans[i] << " ";
     }
-
-    cout << "(";
-    sort(output.begin(), output.end());
-    for (int i = 0; i < output.size()-1; i ++){
-        cout << output.at(i) << ",";
-    }
-    cout << output.at(output.size()-1);
-    cout << ")";
-
-
-
 }
-
-vector<pair<int, int>> activityselection(vector<int> s, vector<int> e, int n){
-    vector<pair<int, int>> act;
-    for(int i = 0; i < s.size(); i ++){
-        pair<int, int> temp = make_pair(s[i], e[i]);
-        act.push_back(temp);
+vector<int> actselection(int N, int* s, int* e){
+    int cur_finish = -1;
+    vector<pair<int, int> > act;
+    vector<pair<int, int> > select;
+    vector<int> sltinx;
+    for (int i = 0; i < N; i ++){
+        act.push_back(make_pair(s[i], e[i]));
     }
-    sort(act.begin(), act.end(), sortdesc);
-    vector<pair<int, int>> ans;
-    /*
-    for (int i = 0; i < act.size(); i ++){
-        cout << act[i].first << " @ " << act[i].second << endl;
-    }
-    */
-
-    ans.push_back(act[0]);
-    int current_start = act[0].first;
-    for (int i = 1; i < s.size(); i ++){
-        if (act[i].second <= current_start){
-            ans.push_back(act[i]);
-            current_start = act[i].first;
+    sort(act.begin(), act.end(), sortbysec);
+    for (int i = 0; i < N; i ++){
+        if (act[i].first >= cur_finish){
+            select.push_back(act[i]);
+            cur_finish = act[i].second;
         }
     }
-    reverse(ans.begin(), ans.end());
-
-    return ans;
-
-
+    for (int i = 0; i < select.size(); i ++){
+        for (int j = 0; j < N; j ++){
+            if (select[i].first == s[j] && select[i].second == e[j]){
+                sltinx.push_back(j+1);
+                break;
+            }
+        }
+    }
+    return sltinx;
 }
