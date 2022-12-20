a2e4476589d608a272ffaebd9e4e3ffb602f0b8f
Ray Hsu <71971775+wavelolz@users.noreply.github.com>
2022-12-20 11:54:38 +0800

    Update huffman coding.cpp

--- huffman coding.cpp
+++ huffman coding.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
-#include <utility>
 #include <bits/stdc++.h>
-#include <algorithm>
 #include <vector>
+
 using namespace std;
+map<int, string> ans;
 
 class Node{
 public:
     int freq;
     Node* left = NULL;
     Node* right = NULL;
-    void print_node(){
-        cout << "freq :" << freq << endl;
-    }
 };
 
 Node* create_node(int freq);
-vector<Node*> extract_min(vector<Node*> prior_que);
-vector<pair<int, vector<char> >> inorder(Node* root, char codeadd, vector<char> code, vector<pair<int, vector<char> >> &output);
-// bool sortans(pair<int, vector<char> > a, pair<int, vector<char> > b){return (a.first < b.first);}
+vector<Node*> huffman(vector<Node*> val);
+bool sortbyfreq(Node* a, Node* b){return (a->freq <= b->freq);}
+void encode(Node* tree, string str);
 
 int main(){
     int N;
@@ -27,126 +24,40 @@ int main(){
     for (int i = 0; i < N; i ++){
         cin >> freq[i];
     }
-
-
-
-    vector<Node*> prior_que;
-    Node* root;
+    vector<Node*> vec(N);
     for (int i = 0; i < N; i ++){
-        root = create_node(freq[i]);
-        prior_que.push_back(root);
+        vec[i] = new Node();
+        vec[i]->freq = freq[i];
+    }
+    while (vec.size() > 1){
+        vec = huffman(vec);
     }
 
-
-    while (prior_que.size() > 1){
-        prior_que = extract_min(prior_que);
-    }
-
-    vector<char> code;
-    vector<pair<int, vector<char> >> output;
-    output = inorder(prior_que[0], '\0', code, output);
-
-    vector<pair<int, vector<char> >> ans;
-    ans.resize(N);
-    int target;
-    int pos;
-
-    for(int i = 0; i < N; i ++){
-        target = output.at(i).first;
-        auto it = find(freq, freq+N, target);
-        pos = distance(freq, it);
-        ans.at(pos) = output.at(i);
-    }
-
+    encode(vec[0], "");
     cout << N << endl;
     for (int i = 0; i < N; i ++){
-        for (int j = 0; j < ans.at(i).second.size(); j ++){
-            cout << ans.at(i).second.at(j);
-        }
+        cout << ans[freq[i]] << " ";
     }
-
-
-
-
-
-
-    return 0;
+}
+Node* create_node(int freq, Node* left, Node* right){
+    Node* root = new Node();
+    root->freq = freq;
+    root->left = left;
+    root->right = right;
+    return root;
+}
+vector<Node*> huffman(vector<Node*> val){
+    sort(val.begin(), val.end(), sortbyfreq);
+    vector<Node*>::iterator it = val.begin();
+    Node* mergenode = create_node(val[0]->freq + val[1]->freq, val[0], val[1]);
+    val.erase(it);
+    val.erase(it);
+    val.push_back(mergenode);
+    return val;
 }
 
-
-Node* create_node(int freq){
-    Node* newnode = new Node();
-    newnode->freq = freq;
-    newnode->left = NULL;
-    newnode->right = NULL;
-    return newnode;
+void encode(Node* tree, string str){
+    if (tree->left == NULL && tree->right == NULL) ans.insert(pair<int, string>(tree->freq, str));
+    if (tree->left) encode(tree->left, str + "0");
+    if (tree->right) encode(tree->right, str + "1");
 }
-
-vector<Node*> extract_min(vector<Node*> prior_que){
-    // 找 freq 最小的位置
-    int mininx = 0;
-    int mintemp = prior_que.at(0)->freq;
-    for (int i = 1; i < prior_que.size(); i ++){
-        if (prior_que.at(i)->freq < mintemp){
-            mininx = i;
-            mintemp = prior_que.at(i)->freq;
-        }
-    }
-
-    // 將 freq 最小的 node 的 pointer 存起來
-    Node* minfreq = prior_que.at(mininx);
-    // 將 freq 最小的 node 的 pointer 從 queue 中移除
-    vector<Node*>::iterator i = prior_que.begin() + mininx;
-    prior_que.erase(i);
-
-
-    // 找 freq 第二小的位置
-    int semininx = 0;
-    int semintemp = prior_que.at(0)->freq;
-    for (int i = 1; i < prior_que.size(); i ++){
-        if (prior_que.at(i)->freq < semintemp){
-            semininx = i;
-            semintemp = prior_que.at(i)->freq;
-        }
-    }
-    Node* seminfreq = prior_que.at(semininx);
-    vector<Node*>::iterator j = prior_que.begin() + semininx;
-    prior_que.erase(j);
-
-    // 將最小和第二小的 node 重新合成一個新的 node
-    Node* mergenode = new Node();
-    mergenode->freq = minfreq->freq + seminfreq->freq;
-    mergenode->left = minfreq;
-    mergenode->right = seminfreq;
-
-    // 將新的 node 存到 queue 裡
-    prior_que.push_back(mergenode);
-
-    return prior_que;
-
-}
-
-vector<pair<int, vector<char> >> inorder(Node* root, char codeadd, vector<char> code, vector<pair<int, vector<char> >> &output){
-    code.push_back(codeadd);
-
-    // 如果是 leaf node 就 output
-    if (!root->left && !root->right){
-        pair<int, vector<char> > ans = make_pair(root->freq, code);
-        output.push_back(ans);
-    }
-
-    if (root->left){
-        inorder(root->left, '0', code, output);
-    }
-
-    if (root->right){
-        inorder(root->right, '1', code, output);
-    }
-    // sort(output.begin(), output.end(), sortans);
-    return output;
-
-
-
-
-}
-
