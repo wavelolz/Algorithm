a5ad38893b5537bab83d74ced2820d2b249703e6
Ray Hsu <71971775+wavelolz@users.noreply.github.com>
2022-12-26 11:03:08 +0800

    Update rod cut dp.cpp

--- rod cut dp.cpp
+++ rod cut dp.cpp
@@ -1,52 +1,51 @@
 #include <iostream>
-#include <climits>
+#include <bits/stdc++.h>
 #include <math.h>
 using namespace std;
 
-int** rodcut(int* arr, int N);
+void rodcut(int* val, int N);
 int main(){
-    int N, current_len, c = 0;
+    int N;
     cin >> N;
-    int arr[N], cutway[N];
+    int val[N];
     for (int i = 0; i < N; i ++){
-        cin >> arr[i];
+        cin >> val[i];
     }
-    int** result;
-    result = rodcut(arr, N);
-    current_len = N;
-    while (current_len > 0){
-        cutway[c] = result[1][current_len-1];
-        current_len -= result[1][current_len-1];
-        c += 1;
-    }
-    cout << result[0][N-1] << endl;
-    cout << c << endl;
-    cout << N << "=";
-    for (int i = 0; i < c-1; i ++){
-        cout << cutway[i] << "+";
-    }
-    cout << cutway[c-1];
+    rodcut(val, N);
 }
 
-int** rodcut(int* arr, int N){
-    int maxprice, rodlen, temp;
-    int** result = new int* [N];
-    for (int i = 0; i < N; i ++){
-        result[i] = new int [N];
+void rodcut(int* val, int N){
+    int maxprice, temp, total_price = 0, cur_len = N;
+    vector<int> cutway;
+    int** arr = new int* [2];
+    for (int i = 0; i < 2; i ++){
+        arr[i] = new int [N];
     }
     for (int i = 0; i < N; i ++){
-        result[1][i] = i+1;
+        arr[0][i] = val[i];
+        arr[1][i] = i+1;
     }
-    result[0][0] = arr[0];
     for (int i = 1; i < N; i ++){
-        maxprice = arr[i];
+        maxprice = arr[0][i];
         for (int j = 1; j <= floor((i+1) / 2); j ++){
             temp = maxprice;
-            maxprice = max(maxprice, arr[j-1] + arr[i-j]);
-            if (temp != maxprice){result[1][i] = j;}
+            maxprice = max(maxprice, val[j-1] + arr[0][i-j]);
+            if (temp != maxprice) arr[1][i] = j;
         }
-        result[0][i] = maxprice;
+        arr[0][i] = maxprice;
     }
-    return result;
+    while (cur_len > 0){
+        cutway.push_back(arr[1][cur_len-1]);
+        cur_len -= arr[1][cur_len-1];
+    }
+    for (int i = 0; i < cutway.size(); i ++){
+        total_price += arr[0][cutway[i]-1];
+    }
+    cout << total_price << endl;
+    cout << cutway.size() << endl;
+    cout << N << " = ";
+    for (int i = 0; i < cutway.size()-1; i ++){
+        cout << cutway[i] << "+";
+    }
+    cout << cutway[cutway.size()-1];
 }
-
